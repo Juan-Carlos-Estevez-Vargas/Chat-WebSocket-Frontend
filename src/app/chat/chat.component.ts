0a814f09bf523a5db4a8a8c1ac75e139be601e3c
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-02-24 15:32:15 -0500

    Adding bootstrap styles, and connect to unconnect methods

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  private client: Client;
+  connected: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.client = new Client();
+
+    /**
+     * Se encarga de añadir el SockJS al Stomp con el broker de mensajería
+     */
+    this.client.webSocketFactory = () => {
+      return new SockJS("http://localhost:8080/chat-websocket");
+    };
+
+    /**
+     * Conecta al usuario al chat
+     */
+    this.client.onConnect = (frame) => {
+      this.connected = true;
+    }
+
+    /**
+     * Desconecta el usuario del chat
+     */
+    this.client.onDisconnect = (frame) => {
+      this.connected = false;
+    }
+
+  }
+
+  /**
+   * Activa la conexión con el broker de mensajería
+   */
+  connect(): void {
+    this.client.activate();
+  }
+
+  /**
+   * Desactiva la conexión con el broker de mensajería
+   */
+  unconnect(): void {
+    this.client.deactivate();
   }
 
 }
